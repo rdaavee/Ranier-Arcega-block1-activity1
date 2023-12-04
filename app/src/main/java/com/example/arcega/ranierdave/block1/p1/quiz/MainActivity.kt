bee8871231fb23dd747a121d4e33660ddb3d171d
Ranier Arcega <rdavee0412@gmail.com>
2023-12-04 10:35:59 +0800

    first commit

---
+++ app/src/main/java/com/example/arcega/ranierdave/block1/p1/quiz/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.arcega.ranierdave.block1.p1.quiz
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.arcega.ranierdave.block1.p1.quiz.ui.theme.RanierArcegaBlock1QuizTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
