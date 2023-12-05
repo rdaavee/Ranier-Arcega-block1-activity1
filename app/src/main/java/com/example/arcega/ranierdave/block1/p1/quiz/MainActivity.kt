06082d3a6f8d2ad8f15f4f5911a002714b7d24f8
Ranier Arcega <rdavee0412@gmail.com>
2023-12-05 20:52:23 +0800

    added Login Button listener

--- app/src/main/java/com/example/arcega/ranierdave/block1/p1/quiz/MainActivity.kt
+++ app/src/main/java/com/example/arcega/ranierdave/block1/p1/quiz/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.arcega.ranierdave.block1.p1.quiz
 
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,9 +17,30 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.arcega.ranierdave.block1.p1.quiz.ui.theme.RanierArcegaBlock1QuizTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var loginEmail : EditText
+    private lateinit var loginPassword : EditText
+    private lateinit var loginBtn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
+        loginEmail = findViewById(R.id.inputEmail)
+        loginPassword = findViewById(R.id.inputPassword)
+        loginBtn = findViewById(R.id.loginButton)
+
+        loginBtn.setOnClickListener {
+
+            val insertedEmail = loginEmail.text.toString()
+            val insertedPass = loginPassword.text.toString()
+
+            if (TextUtils.isEmpty(insertedEmail) || TextUtils.isEmpty(insertedPass)) {
+                Toast.makeText(this, "Add Email & Password!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Wrong Credentials!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
